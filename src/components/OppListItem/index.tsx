9356862353f57648e94fb63fb40d0636f6aa3065
Marcos Marques <marcosmarquesdev@gmail.com>
2021-04-29 19:41:29 -0300

    Adiciona OppListItem

---
+++ src/components/OppListItem/index.tsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from 'react';
+
+import useAuth from '../../contexts/auth/useAuth';
+import useUi from '../../contexts/ui/useUi';
+
+import { MaterialIcons as Icon } from '@expo/vector-icons';
+import { TypeOpp } from '../../models/opp';
+
+import { TouchableOpacity } from 'react-native';
+import {
+    Container,
+    HeaderView,
+    RatingView,
+    LblRating,
+    Image,
+    LblName,
+    Content,
+} from './styles';
+
+type TypeProps = {
+    opp: TypeOpp;
+    onFavorite(): void;
+    onPress(): void;
+};
+
+const OppCard: React.FC<TypeProps> = props => {
+    const { opp, onFavorite, onPress } = props;
+    const { user } = useAuth();
+    const { theme, strings } = useUi();
+
+    const [favorited, setFavorited] = useState<boolean>(false);
+
+    useEffect(() => {
+        setFavorited(user!.favoritesOpportunities.includes(opp.id));
+    }, [user]);
+
+    return (
+        <Container>
+            <Image source={{ uri: opp.photos[0] }} />
+            <Content>
+                <LblName>{opp.name}</LblName>
+                <HeaderView>
+                    <RatingView>
+                        <Icon
+                            name="star"
+                            size={30}
+                            color={theme.colors.rating}
+                        />
+                        <LblRating>{opp.rating}</LblRating>
+                    </RatingView>
+                    <TouchableOpacity onPress={onFavorite}>
+                        <Icon
+                            name={favorited ? 'favorite' : 'favorite-border'}
+                            size={30}
+                            color={theme.colors.secondary}
+                        />
+                    </TouchableOpacity>
+                </HeaderView>
+            </Content>
+        </Container>
+    );
+};
+
+export default OppCard;
---
+++ src/components/OppListItem/styles.ts
@@ -0,0 +1,50 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.TouchableOpacity`
+    width: 100%;
+    height: 100px;
+    margin-bottom: 10px;
+    background-color: ${props => props.theme.colors.section};
+    flex-direction: row;
+`;
+
+export const Content = styled.View`
+    flex: 1;
+    justify-content: space-between;
+    padding: 6px;
+`;
+
+export const HeaderView = styled.View`
+    flex-direction: row;
+    justify-content: space-between;
+`;
+
+export const RatingView = styled.View`
+    flex-direction: row;
+    align-items: center;
+`;
+
+export const LblRating = styled.Text`
+    font-family: RobotoMono;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 18px;
+    line-height: 24px;
+    color: ${props => props.theme.colors.font.main};
+    margin-left: 5px;
+`;
+
+export const Image = styled.Image`
+    width: 100px;
+    height: 100px;
+`;
+
+export const LblName = styled.Text`
+    font-family: RobotoMono;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 20px;
+    line-height: 37px;
+    letter-spacing: 0.2px;
+    color: ${props => props.theme.colors.font.darker};
+`;
